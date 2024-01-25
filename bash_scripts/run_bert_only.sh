a66e1e7f95f6899d758d7162e4e3312f11c96d93
Maksim <maxdzhigil@gmail.com>
2024-01-25 18:53:11 +0500

    add bash scripts to run experiments

---
+++ bash_scripts/run_bert_only.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 ../run_experiments.py -n 10 --regenerate_requests --run_bert
---
+++ bash_scripts/run_gzip_only.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 ../run_experiments.py -d request_data/ -n 10 --regenerate_requests --run_gzip
